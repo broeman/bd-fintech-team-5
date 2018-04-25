1dd8a4443e064e46dabbf7937ab92572dcb05e89
nicolai4321 <nicolai4321@gmail.com>
2018-04-25 17:31:40 +0200

    grafen vises flere gange

--- angular4/src/pages/overview/overview.ts
+++ angular4/src/pages/overview/overview.ts
@@ -80,6 +80,10 @@ export class Overview {
 
     private dataReady(data){
         this.accounts = data.accounts;
+        this.chartData = {
+            names: [],
+            balances: []
+        }
         for(let i = 0; i < this.accounts.length; i++){
             this.chartData.names.push(this.accounts[i].name);
             this.chartData.balances.push(this.accounts[i].balance);
